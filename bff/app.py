f226fc4525131c87f5773c41c552feb7fd10a476
David Cloutman <david.cloutman@lampfire.com>
2021-02-09 23:16:03 -0800

    Many advancement towards a working layout including header improvements. More styles with shadows. Beginnings of decomposition into seperate pages with independent routes. Added a login stub.

--- bff/app.py
+++ bff/app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
+import smtplib
 
 app = Flask(__name__)
 
 @app.route('/')
 def home ():
-	return render_template("home.jinja", page_headline="Site Template 2021")
+	return render_template("home.jinja", page_headline="Site Template 2021", show_page_aside=True)
 
 @app.route('/about')
 def about():
-	return "About"
+	return render_template("about.jinja", page_headline="About Us")
 
 @app.route('/contact')
 def contact():
-	return "Contact"
+	return render_template("contact.jinja", page_headline="Contact Us")
 
 @app.route('/products')
 def products():
 	return "Products"
 
+@app.route('/login')
+def login():
+	return "Login"
 if __name__ == "app.py":
 	app.run()
