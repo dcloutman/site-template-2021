87180d987795e91aba9ba840d66b197a134fba02
David Cloutman <david.cloutman@lampfire.com>
2021-02-21 19:14:30 -0800

    Improved lightbox functionality in main.js.

--- bff/static/js/main.js
+++ bff/static/js/main.js
@@ -1,27 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     (function () {
-        const start_time = (new Date()).getSeconds();
-        const num_seconds_to_email_display = 5;
-        const close_button = document.getElementById('overlay_content_email_signup_close_button');
+        const start_time = (new Date()).getTime();
+        const num_milliseconds_until_email_lightbox = 10000;
+        const close_button = document.getElementById('lightbox_content_email_signup_close_button');
 
-        console.log(close_button);
 
         const email_display_interval = setInterval(function () {
-            if (((new Date()).getSeconds() - num_seconds_to_email_display) > num_seconds_to_email_display) {
-                document.body.classList.add("active_overlay");
-                document.body.classList.add("email_signup");
-                document.body.classList.add("blurry_face");
+            let do_not_show_email_lightbox_until = null;
+            if (((new Date()).getTime() - start_time) > num_milliseconds_until_email_lightbox) {
+                do_not_show_email_lightbox_until = window.localStorage.getItem("do_not_show_email_lightbox_until");
 
+                if (
+                    -1 !== do_not_show_email_lightbox_until &&
+                    (
+                        null === do_not_show_email_lightbox_until ||
+                        (new Date()).getTime() >= do_not_show_email_lightbox_until
+                    )
+                ) {
+                    document.body.classList.add("active_lightbox");
+                    document.body.classList.add("email_signup");
+                    document.body.classList.add("blurry_face");
 
-                clearInterval(email_display_interval);
+                    clearInterval(email_display_interval);
+                }
             }
-        }, 1000);
+        }, 10000);
 
-        close_button.addEventListener('click', function (e) {
-            document.body.classList.remove("active_overlay");
+        /* Close email signup lightbox without submission. */
+        close_button.addEventListener("click", function (e) {
+            document.body.classList.remove("active_lightbox");
             document.body.classList.remove("email_signup");
             document.body.classList.remove("blurry_face");
 
+            // Add a record to local storage to prevent displaying popup for one day.
+            // window.localStorage.setItem("do_not_show_email_lightbox_until", (new Date).getTime() + (1000 * 60)); // DEBUG: Display again after one minute.
+            window.localStorage.setItem("do_not_show_email_lightbox_until", (new Date).getTime() + (1000 * 60 * 60 * 24));
 
             e.stopPropagation();
         });
