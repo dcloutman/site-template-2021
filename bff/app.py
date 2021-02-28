aadca7461c0e9ad04b1c626ad05703b94550f484
David Cloutman <david.cloutman@lampfire.com>
2021-02-28 00:18:35 -0800

    The global variables db and app are now exposed to the application library. This should allow the SQLAlchemy models to properly function.

--- bff/app.py
+++ bff/app.py
@@ -1,48 +1,25 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import smtplib
+from flask import render_template
+from bff import app
+from bff import db
+
 from bff.views.home_view import HomeView
 from bff.views.contact_view import ContactView
 from bff.views.about_view import AboutView
 from bff.views.icons_view import IconsView
-import os
+from bff.views.login_view import LoginView
 
-app = Flask(__name__)
-
-
-# Get database config.
-db_username = ''
-if 'DB_USERNAME' in os.environ:
-    db_username = os.environ['DB_USERNAME']
-
-db_password = ''
-if 'DB_PASS' in os.environ:
-    db_password = os.environ['DB_PASS']
-
-db_host = ''
-if 'DB_HOST' in os.environ:
-    db_host = os.environ['DB_HOST']
-
-db_name = ''
-if 'DB_NAME'  in os.environ:
-    db_name = os.environ['DB_NAME']
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_host}/{db_name}'
-db = SQLAlchemy(app)
 
 # Register view classes with the Flask app.
 HomeView.register(app)
 ContactView.register(app)
 AboutView.register(app)
 IconsView.register(app)
+LoginView.register(app)
 
 @app.route('/products')
 def products():
     return "Products"
-
-#@app.route('/icons')
-#def icons():
-#    return render_template("icons.jinja")
+ 
 
 @app.route('/login')
 def login():
@@ -50,3 +27,4 @@ def login():
 
 if __name__ == "app.py":
     app.run()
+
---
+++ bff/bff/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+"""
+Initialize app and db globals.
+"""
+LIBARY_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT =  os.path.join(LIBARY_PATH, os.pardir)
+STATIC_PATH = os.path.join(os.pardir, "static")
+
+app = Flask(__name__, template_folder=os.path.join(PROJECT_ROOT, "templates"), static_folder=STATIC_PATH)
+
+def initialize_db(app):
+    # Get database config.
+    db_username = ''
+    if 'DB_USERNAME' in os.environ:
+        db_username = os.environ['DB_USERNAME']
+
+    db_password = ''
+    if 'DB_PASS' in os.environ:
+        db_password = os.environ['DB_PASS']
+
+    db_host = ''
+    if 'DB_HOST' in os.environ:
+        db_host = os.environ['DB_HOST']
+
+    db_name = ''
+    if 'DB_NAME' in os.environ:
+        db_name = os.environ['DB_NAME']
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_host}/{db_name}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Run initialization in private namespace.
+initialize_db(app)
+db = SQLAlchemy(app)
