43616839462a5001c0ab16dd9473dd8bef1c099a
David Cloutman <david.cloutman@lampfire.com>
2021-03-26 21:47:28 -0700

    Removed unnecessary library imports. Added comments explaining the Flask Classful architecture a bit so that critical design decisions aren't mysterious or based upon an expectation of inferred understanding by the code's users.

--- bff/app.py
+++ bff/app.py
@@ -1,6 +1,4 @@
-from flask import render_template
-from bff import app
-from bff import db
+from bff import app # Triggers the bff/__init__.py
 
 from bff.views.home_view import HomeView
 from bff.views.contact_view import ContactView
@@ -9,6 +7,15 @@ from bff.views.icons_view import IconsView
 from bff.views.login_view import LoginView
 from bff.views.admin_emails_view import AdminEmailsView
 from bff.views.admin_email_lists_view import AdminEmailListsView
+
+"""
+See bff/__init__.py for the initialization of `app` and the importation of application
+configurations. This allow the libraries to share the app variable, which is where most
+of the interesting stuff happens. The directory bff/views contains view controllers 
+written using the excellent Flask Classful library, which nicely structures the
+`@app.route` functions usually found in this file into classes with methods for each
+HTTP method.
+"""
 
 # Register view classes with the Flask app.
 HomeView.register(app)
